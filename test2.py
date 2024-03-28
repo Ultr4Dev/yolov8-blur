6f1f1d24e39e3c0fbab5485adfa711eb89280c65
Casper Medin Jensen <casper.jensen@hotmail.se>
2024-03-28 19:32:52 +0100

    i updated the gitignore file and added coco-classes.yaml and also updated the test2.py

--- test2.py
+++ test2.py
@@ -6,21 +6,22 @@ from ultralytics import YOLO, SAM
 from pathlib import Path
 
 # Initialize YOLO model
-yolo_model = YOLO("yolov8x-seg.pt")
+yolo_model = YOLO("models/yolov8x-seg.pt")
 predict_pose = True
 pixelate_state = True
 
 if predict_pose:
-    pose_model = YOLO("yolov8x-pose.pt")
-
+    pose_model = YOLO("models/yolov8x-pose.pt")
+    pose_model.conf = 0.25
+    pose_model.cuda()
 
 # Initialize virtual camera
-with pyvirtualcam.Camera(width=1920, height=1080, fps=10, device="OBS Virtual Camera", fmt=pyvirtualcam.PixelFormat.BGR) as cam:
+with pyvirtualcam.Camera(width=1920, height=1080, fps=30, device="OBS Virtual Camera", fmt=pyvirtualcam.PixelFormat.BGR) as cam:
     yolo_model.cuda()
 
     # Predict on video stream
     results = yolo_model.predict(
-        source="0", stream=True, conf=0.30, classes=[0, 62])
+        source="0", stream=True, conf=0.35)
     for r in results:
 
         img = np.copy(r.orig_img)
@@ -31,45 +32,55 @@ with pyvirtualcam.Camera(width=1920, height=1080, fps=10, device="OBS Virtual Ca
             label = c.boxes[0].cls.cpu().tolist()[0]
             # Convert label to string
             label = c.names[int(label)]
-            if label != "person":
-                print(label)
+            if label == "person":
+                b_mask = np.zeros(img.shape[:2], np.uint8)
+
+                # Create contour mask
+                contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
+                _ = cv2.drawContours(
+                    b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
+                isolated = np.dstack([img, b_mask])
+
+                if predict_pose:
+                    pose_results = pose_model(isolated[:, :, :3])
+
+                # Pixelate isolated object
+                if pixelate_state:
+                    try:
+                        w, h = isolated[:, :, 2].shape[::-1]
+                        isolated = isolated[:, :, :3]
+
+                        # Resize the isolated object
+                        isolated = cv2.resize(
+                            isolated, (w//25, h//25), interpolation=cv2.INTER_NEAREST)
+
+                        # Resize the isolated object back to original size
+                        isolated = cv2.resize(
+                            isolated, (1920, 1080), interpolation=cv2.INTER_NEAREST)
+
+                    except:
+                        continue
+
+                if predict_pose:
+                    for pose in pose_results:
+                        x = pose.plot(
+                            img=isolated[:, :, :3], show=False, conf=False, boxes=False)
+                        isolated = x
+
+                # Replace object with blurred version
+                img[b_mask > 0] = isolated[b_mask > 0]
+            if label in ["tv", "laptop", "cell phone"]:
+                # Draw a black polygon over the TV
+                b_mask = np.zeros(img.shape[:2], np.uint8)
+                contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
+                _ = cv2.drawContours(
+                    b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
+                img[b_mask > 0] = 0
+            else:
                 continue
-            b_mask = np.zeros(img.shape[:2], np.uint8)
-
-            # Create contour mask
-            contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
-            _ = cv2.drawContours(
-                b_mask, [contour], -1, (255, 255, 255), cv2.FILLED)
-            isolated = np.dstack([img, b_mask])
-
-            if predict_pose:
-                pose_results = pose_model(isolated[:, :, :3])
-
-            # Pixelate isolated object
-            if pixelate_state:
-                try:
-                    w, h = isolated[:, :, 2].shape[::-1]
-                    isolated = isolated[:, :, :3]
-
-                    # Resize the isolated object
-                    isolated = cv2.resize(
-                        isolated, (w//25, h//25), interpolation=cv2.INTER_NEAREST)
-
-                    # Resize the isolated object back to original size
-                    isolated = cv2.resize(
-                        isolated, (1920, 1080), interpolation=cv2.INTER_NEAREST)
-
-                except:
-                    continue
-
-            if predict_pose:
-                for pose in pose_results:
-                    x = pose.plot(
-                        img=isolated[:, :, :3], show=False, conf=False, boxes=False)
-                    isolated = x
-
-            # Replace object with blurred version
-            img[b_mask > 0] = isolated[b_mask > 0]
+        # Rezise the image to 720p
+        preview = cv2.resize(img, (1920, 1080))
+        cv2.imshow("preview", preview)
 
         cam.send(img)
         cam.sleep_until_next_frame()
