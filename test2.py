c72cbf43efac70d30c6d87fe5fe3d0af41ca9edf
Casper Medin Jensen <casper.jensen@hotmail.se>
2024-03-29 05:00:05 +0100

    Blur people and hide tv's

--- test2.py
+++ test2.py
@@ -6,35 +6,17 @@ from pathlib import Path
 import time
 
 
-def list_devices():
-    devices = []
-    for i in range(10):
-        try:
-            cap = cv2.VideoCapture(i)
-            if not cap.isOpened():
-                continue
-            ret, frame = cap.read()
-            if not ret:
-                continue
-            cv2.imshow(f"Camera {i}", frame)
-            devices.append(i)
-            cap.release()
-            cv2.waitKey(1)
-        except:
-            continue
-
-    return devices
-
-
-def select_device():
-    devices = list_devices()
-    print("Select camera device:")
-    device = int(input("Available devices: " +
-                 ", ".join(map(str, devices)) + "\n"))
-    cv2.destroyAllWindows()
-    return device
-
-
+inpaint_state = False
+labels = ["bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+          "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+          "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+          "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+          "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+          "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+          "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard",
+          "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+          "scissors", "teddy bear", "hair drier", "toothbrush"
+          ]
 # Initialize YOLO model
 yolo_model = YOLO("models/yolov8x-seg.pt")
 yolo_model.cuda()
@@ -42,56 +24,84 @@ predict_pose = False
 pixelate_state = True
 
 # Initialize virtual camera
-with pyvirtualcam.Camera(width=1920, height=1080, fps=20, device="OBS Virtual Camera", fmt=pyvirtualcam.PixelFormat.BGR) as cam:
+with pyvirtualcam.Camera(width=1920, height=1080, fps=30, fmt=pyvirtualcam.PixelFormat.BGR) as cam:
     start_time = time.time()
     frame_count = 0
 
     # Predict on video stream
     results = yolo_model.predict(
-        source=0, stream=True, conf=0.30, classes=[0, 62, 63, 67])
+        source=3, stream=True, conf=0.25, classes=[0, 62])
     for r in results:
         frame_count += 1
         img = np.copy(r.orig_img)
         # Initialize a combined mask for all persons
         combined_mask = {}
-
         for ci, c in enumerate(r):
             label = c.names[int(c.boxes[0].cls.cpu().tolist()[0])]
+
             if label not in combined_mask:
-                combined_mask[label] = np.zeros_like(img[:, :, 0])
+                combined_mask[label] = {}
+                combined_mask[label]["combined"] = np.zeros_like(img[:, :, 0])
+                combined_mask[label]["individual"] = {}
+
+            if ci not in combined_mask[label]["individual"]:
+                combined_mask[label]["individual"][ci] = np.zeros_like(
+                    img[:, :, 0])
             if label == "person":
-                # Create contour mask
-                contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
-                cv2.drawContours(
-                    combined_mask[label], [contour], -1, (255, 255, 255), cv2.FILLED)
 
-            if label in ["tv", "laptop", "cell phone"]:
+                contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
+
+                # Create contour mask
+                cv2.drawContours(
+                    combined_mask[label]["combined"], [contour], -1, (255, 255, 255), cv2.FILLED)
+                cv2.drawContours(
+                    combined_mask[label]["individual"][ci], [contour], -1, (255, 255, 255), cv2.FILLED)
+
+            if label in labels:
                 # Create contour mask
                 contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
                 cv2.drawContours(
-                    combined_mask[label], [contour], -1, (255, 255, 255), cv2.FILLED)
+                    combined_mask[label]["combined"], [contour], -1, (255, 255, 255), cv2.FILLED)
+                cv2.drawContours(
+                    combined_mask[label]["individual"][ci], [contour], -1, (255, 255, 255), cv2.FILLED)
+
         # Pixelate all people in the image
-        if pixelate_state and np.sum(combined_mask["person"]) > 0:
-            isolated = cv2.bitwise_and(img, img, mask=combined_mask["person"])
+        try:
 
-            # Combine all person masks from the combined_mask["person"]
-            person_mask = combined_mask["person"]
-            isolated = cv2.bitwise_and(img, img, mask=person_mask)
-            w, h = isolated.shape[1], isolated.shape[0]
-            # Resize -> Pixelate -> Resize back
-            isolated_small = cv2.resize(
-                img, (w//25, h//25), interpolation=cv2.INTER_NEAREST)
-            isolated_large = cv2.resize(
-                isolated_small, (w, h), interpolation=cv2.INTER_NEAREST)
+            if pixelate_state and np.sum(combined_mask["person"]["combined"]) > 0:
+                # Combine all person masks from the combined_mask["person"]
+                person_mask = combined_mask["person"]["combined"]
+                isolated = cv2.bitwise_and(img, img, mask=person_mask)
+                w, h = isolated.shape[1], isolated.shape[0]
+                # Resize -> Pixelate -> Resize back
+                isolated_small = cv2.resize(
+                    img, (w//10, h//10), interpolation=cv2.INTER_NEAREST)
+                isolated_small = cv2.blur(isolated_small, (20, 20))
+                isolated_large = cv2.resize(
+                    isolated_small, (w, h), interpolation=cv2.INTER_NEAREST)
+                img[person_mask >
+                    0] = isolated_large[person_mask > 0]
 
-            img[combined_mask["person"] >
-                0] = isolated_large[combined_mask["person"] > 0]
+        except:
+            pass
 
         # Draw black polygon for all tv's, laptops, and cell phones in the combined mask
-        for label in ["tv", "laptop", "cell phone"]:
+        for label in labels:
             try:
-                if np.sum(combined_mask[label]) > 0:
-                    img[combined_mask[label] > 0] = 0
+                if np.sum(combined_mask[label]["combined"]) > 0:
+                    img[combined_mask[label]["combined"] > 0] = 0
+                    # Calculate the center of the mask and add label text
+                for ci, mask in combined_mask[label]["individual"].items():
+                    if np.sum(mask) > 0:
+                        img[mask > 0] = 0
+                        M = cv2.moments(mask)
+                        if M["m00"] != 0:
+                            # Calculate the center of the mask
+                            cX = int(M["m10"] / M["m00"])
+                            cY = int(M["m01"] / M["m00"])
+                            # Add label text
+                            cv2.putText(img, label, (cX, cY),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
             except:
                 continue
 
@@ -101,15 +111,14 @@ with pyvirtualcam.Camera(width=1920, height=1080, fps=20, device="OBS Virtual Ca
 
         # Add FPS counter to top left corner
         cv2.putText(img, f"FPS: {fps:.2f}", (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         preview = cv2.resize(img, (1280, 720))
         cv2.imshow("preview", preview)
 
         cam.send(img)
         cam.sleep_until_next_frame()
+        cv2.waitKey(1)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
 cam.close()
